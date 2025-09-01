f117fbe492bb2b019e045b3026fcb6410847b61d
diaco <shilahassanabadi@gmail.com>
2025-09-01 21:46:29 +0330

    setup library

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="text-on-image",
+    version="0.1.0",
+    description="Easily add Persian/English text on images with Pillow.",
+    author="Ario",
+    author_email="ario.h.abbadi@gmail.com",
+    url="https://github.com/diaco-dev/text_on_image",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Pillow",
+        "arabic-reshaper",
+        "python-bidi"
+    ],
+    python_requires=">=3.7",
+)
